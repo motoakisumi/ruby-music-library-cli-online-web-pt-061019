c4b671c75365c6e78240918da3f31ec559e402e0
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-08-19 04:55:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,9 +3,10 @@ class Song
   attr_accessor :name
   @@all = []
 
-  def initialize(name, artist=nil)
+  def initialize(name, artist=nil, genre = nil)
     @name = name
-    @artist = artist if artist
+    self.artist = artist if artist
+    self.genre = genre if genre
   end
 
   def self.all
@@ -17,7 +18,7 @@ class Song
   end
 
   def self.destroy_all
-    @@all = []
+    @@all.clear
   end
 
   def self.create(name)
@@ -32,9 +33,43 @@ class Song
 
   def artist=(artist)
     @artist = artist
-    Artist.add_song(self)
+    @artist.add_song(self)
   end
 
+  def genre
+    @genre
+  end
 
+  def genre=(genre)
+    @genre = genre
+    @genre.add_song(self)
+  end
 
+  def self.find_by_name(name)
+    @@all.detect{|song|song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    result = self.find_by_name(name)
+    if result != nil
+      result
+    else
+      self.create(name)
+    end
+  end
+
+  def self.new_from_filename(filename)
+    name = filename.split(" - ")[1]
+    a = filename.split(" - ")[0]
+    g = filename.split(" - ")[2].gsub(".mp3","")
+
+    artist = Artist.find_or_create_by_name(a)
+    genre = Genre.find_or_create_by_name(g)
+
+    Song.new(name, artist, genre)
+  end
+
+  def self.create_from_filename(filename)
+    new_from_filename(filename).tap{|song| song.save }
+  end
 end
